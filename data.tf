443c1668ec3a4e8742d456334304d96b2d87fb0c
Aram Karapetyan <aram@dasmeta.com>
2022-12-20 13:46:27 +0400

    Splite manifests

--- data.tf
+++ data.tf
@@ -6,6 +6,22 @@ data "aws_iam_openid_connect_provider" "this" {
   url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
 
-data "kubectl_file_documents" "docs" {
-  content = file("${path.module}/files/addons-otel-permissions.yaml")
+data "kubectl_file_documents" "clusterrole" {
+  content = file("${path.module}/files/ClusterRole.yaml")
+}
+
+data "kubectl_file_documents" "clusterrolebinding" {
+  content = file("${path.module}/files/ClusterRoleBinding.yaml")
+}
+
+data "kubectl_file_documents" "rolebinding" {
+  content = file("${path.module}/files/RoleBinding.yaml")
+}
+
+data "kubectl_file_documents" "role" {
+  content = file("${path.module}/files/Role.yaml")
+}
+
+data "kubectl_file_documents" "namespace" {
+  content = file("${path.module}/files/Namespace.yaml")
 }
--- operator-permissions.tf
+++ operator-permissions.tf
@@ -7,9 +7,32 @@ resource "kubernetes_namespace" "operator" {
   }
 }
 
-resource "kubectl_manifest" "this" {
-  for_each   = data.kubectl_file_documents.docs.manifests
-  yaml_body  = each.value
+resource "kubectl_manifest" "namespace" {
+  yaml_body  = data.kubectl_file_documents.namespace.manifests
   apply_only = true
   depends_on = [kubernetes_namespace.operator]
+}
+
+resource "kubectl_manifest" "clusterrole" {
+  yaml_body  = data.kubectl_file_documents.clusterrole.manifests
+  apply_only = true
+  depends_on = [kubernetes_namespace.operator, kubectl_manifest.namespace]
+}
+
+resource "kubectl_manifest" "clusterrolebinding" {
+  yaml_body  = data.kubectl_file_documents.clusterrolebinding.manifests
+  apply_only = true
+  depends_on = [kubernetes_namespace.operator, kubectl_manifest.namespace]
+}
+
+resource "kubectl_manifest" "rolebinding" {
+  yaml_body  = data.kubectl_file_documents.rolebinding.manifests
+  apply_only = true
+  depends_on = [kubernetes_namespace.operator, kubectl_manifest.namespace]
+}
+
+resource "kubectl_manifest" "role" {
+  yaml_body  = data.kubectl_file_documents.role.manifests
+  apply_only = true
+  depends_on = [kubernetes_namespace.operator, kubectl_manifest.namespace]
 }
