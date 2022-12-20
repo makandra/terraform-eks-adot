3fcc3a4dbc40233e9576707bc36b49360cdefea4
Aram Karapetyan <aram@dasmeta.com>
2022-12-20 14:35:29 +0400

    Update module added dependency

--- operator-permissions.tf
+++ operator-permissions.tf
@@ -8,31 +8,31 @@ resource "kubernetes_namespace" "operator" {
 }
 
 resource "kubectl_manifest" "namespace" {
-  yaml_body  = data.kubectl_file_documents.namespace[0].manifests
+  yaml_body  = data.kubectl_file_documents.namespace.manifests[0]
   apply_only = true
   depends_on = [kubernetes_namespace.operator]
 }
 
 resource "kubectl_manifest" "clusterrole" {
-  yaml_body  = data.kubectl_file_documents.clusterrole.manifests
+  yaml_body  = data.kubectl_file_documents.clusterrole.manifests[0]
   apply_only = true
   depends_on = [kubernetes_namespace.operator, kubectl_manifest.namespace]
 }
 
 resource "kubectl_manifest" "clusterrolebinding" {
-  yaml_body  = data.kubectl_file_documents.clusterrolebinding.manifests
+  yaml_body  = data.kubectl_file_documents.clusterrolebinding.manifests[0]
   apply_only = true
   depends_on = [kubernetes_namespace.operator, kubectl_manifest.namespace]
 }
 
 resource "kubectl_manifest" "rolebinding" {
-  yaml_body  = data.kubectl_file_documents.rolebinding.manifests
+  yaml_body  = data.kubectl_file_documents.rolebinding.manifests[0]
   apply_only = true
   depends_on = [kubernetes_namespace.operator, kubectl_manifest.namespace]
 }
 
 resource "kubectl_manifest" "role" {
-  yaml_body  = data.kubectl_file_documents.role.manifests
+  yaml_body  = data.kubectl_file_documents.role.manifests[0]
   apply_only = true
   depends_on = [kubernetes_namespace.operator, kubectl_manifest.namespace]
 }
