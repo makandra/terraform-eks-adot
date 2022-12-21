e26caae839cc2652ecf183dcfc24200f923b7bd5
Claus-Theodor Riegg <claus.riegg@makandra.de>
2022-12-21 15:14:39 +0100

    docs: update examples

---
+++ examples/setup-with-otel-config/provider.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~>1.14"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args = [
+        "eks",
+        "--region", var.region,
+        "get-token", "--cluster-name", var.cluster_name
+      ]
+      command = "aws"
+    }
+  }
+}
